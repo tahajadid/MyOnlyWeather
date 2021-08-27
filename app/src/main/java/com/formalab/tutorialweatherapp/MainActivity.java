a230898cfa78c98235eb31a13acfb8d9fe8e954f
taha jadid <tahajadid98@gmail.com>
2021-08-27 19:20:17 +0100

    Add Location Commit

--- app/src/main/java/com/formalab/tutorialweatherapp/MainActivity.java
+++ app/src/main/java/com/formalab/tutorialweatherapp/MainActivity.java
@@ -1,34 +1,56 @@
 package com.formalab.tutorialweatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String API_KEY = "4e766e5e6026d9658f8974fcd083e6ef";
 
     Button btnSearch;
+    Button btnLocalisation;
     EditText etCityName;
     ImageView iconWeather;
     TextView tvTemp, tvCity;
     ListView lvDailyWeather;
+    LinearLayout MainPart;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +60,18 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         btnSearch = findViewById(R.id.btnSearch);
+        btnLocalisation = findViewById(R.id.btnLocalisation);
         etCityName = findViewById(R.id.etCityName);
         iconWeather = findViewById(R.id.iconWeather);
         tvTemp = findViewById(R.id.tvTemp);
         tvCity = findViewById(R.id.tvCity);
         lvDailyWeather = findViewById(R.id.lvDailyWeather);
+        MainPart = findViewById(R.id.MainPart);
 
+        // Make the Main Part Invisible
+        MainPart.setVisibility(View.INVISIBLE);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,13 +80,66 @@ public class MainActivity extends AppCompatActivity {
                 if (city.isEmpty())
                     Toast.makeText(MainActivity.this, "Please enter a city name", Toast.LENGTH_LONG).show();
                 else {
-
                     loadWeatherByCityName(city);
                 }
             }
         });
 
+        btnLocalisation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    // When permission granted
+                    getLocation();
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this
+                            , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                }
+            }
+        });
+
     }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            Log.d("locc","inside Check !!!!");
+
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<Location> task) {
+
+                Location location = task.getResult();
+
+                if(location != null){
+                    Geocoder geocoder = new Geocoder(MainActivity.this,Locale.getDefault());
+                    try {
+                        List<Address> adress = geocoder.getFromLocation(
+                                location.getLatitude(),location.getLongitude(),1);
+
+                        // Search by the name of the city returned
+                        loadWeatherByCityName(adress.get(0).getLocality());
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this,"Server Error",Toast.LENGTH_SHORT);
+                    }
+
+                }else{
+                    Toast.makeText(MainActivity.this,"Please Turn On Your Localisation !!",Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+
 
     private void loadWeatherByCityName(String city) {
         String apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + API_KEY;
@@ -73,6 +154,10 @@ public class MainActivity extends AppCompatActivity {
                             e.printStackTrace();
                             Toast.makeText(MainActivity.this, "please check your network connection !", Toast.LENGTH_SHORT).show();
                         } else {
+
+                            // Make the Main Part Visible
+                            MainPart.setVisibility(View.VISIBLE);
+
                             // convert json response to java
                             JsonObject main = result.get("main").getAsJsonObject();
                             double temp = main.get("temp").getAsDouble();
@@ -89,6 +174,7 @@ public class MainActivity extends AppCompatActivity {
                             JsonObject coord = result.get("coord").getAsJsonObject();
                             double lon = coord.get("lon").getAsDouble();
                             double lat = coord.get("lat").getAsDouble();
+
                             loadDailyForecast(lon, lat);
                         }
                     }
@@ -132,5 +218,6 @@ public class MainActivity extends AppCompatActivity {
                 .load("http://openweathermap.org/img/w/" + icon + ".png")
                 .intoImageView(iconWeather);
     }
+
 
 }
